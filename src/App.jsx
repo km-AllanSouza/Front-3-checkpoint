9a3f67890f92193fc0b0d5bc36ff0a2e5b2c8460
Allan.Souza <km.allansouza@gmail.com>
2022-11-07 22:52:18 -0300

    front 3 checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,62 @@
 
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+import "./style.scss"
+import { Card } from "./Card"
 
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
 
+    
+  const listaFilme = [
+    {
+      nomeFilme: "filmeTest",
+      filmeUrl: "https://i.ytimg.com/vi/YF1eYbfbH5k/maxresdefault.jpg"
+    }
+  ]
+
+
 
   return (
-    <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* Comece a desenvolver o seu Código por aqui :) */}
-     </form>
-    </div>
+    <main>
+      <div className="App">
+        <h1>Adicionar filme</h1>
+        <form>
+          <div>
+            <label htmlFor="nomeFilme">Nome do Filme </label>
+            <input type="text" id="nomeFilme" />
+          </div>
+          <div>
+            <label htmlFor="capaFilme">Capa do Filme </label>
+            <input type="text" id="capaFilme" />
+          </div>
+          <div>
+            <button type='submit'>Cadastrar Filme</button>
+            <button type='reset'>Limpar formulário</button>
+          </div>
+        </form>
+      </div>
+
+      <div>
+        <h1>Filmes Favoritos</h1>
+        <ul className="filmes-listados">
+          {listaFilme.map(
+            component => {
+              return (
+                < Card
+                  key={component.id}
+                  item={component}
+
+                />
+              )
+
+            }
+          )}
+
+        </ul>
+
+      </div>
+
+    </main>
   )
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,14 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
   return (
-    <div>
+    // <div>
 
-    </div>
+    // </div>
+
+    <li>
+      <h1>{props.item.nomeFilme}</h1>
+      <img src={props.item.filmeUrl}  />
+    </li>
   )
 }
